f6f8dd77705982bb32f35b61e423a197cabb3014
Prashant Solanki <Sanki@prashant.solanki@outlook.in>
2015-02-03 23:46:06 -0500

    reorganizing files updating readme

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 BranchPredictorSimulator
 ========================
+The project implements branch prediction via GSHARE and local history register.
 
-code to simulate branch predition architectures
-=======
-BranchPredictor
-===============
+How to build
+-------------
+```bash
+$> make
+```
+
+How to run unit tests
+----------------
+The script runs the binary with predefined test inputs and diffs the result with expected output files.
+If test are successfull no output will be displayed else difference is printed to the screen.
+```bash
+$>./verify.sh
+```
+
+How to run simulator
+--------------------
+* Make  generates the project binary "sim"
+```bash 
+./sim
+```
+* Application will prompt with list of expected arguments
+* Specify the arguments in correct order and run the applicatio again
+* Results are printed to console hence you can redirect it to a file for future use
+* Have a look at verify.sh for sample inputs
+```bash
+$> ./sim <arguments> > Result.txt
+```
+
+
--- verify.sh
+++ verify.sh
@@ -1,27 +1,31 @@
-./sim bimodal 7 0 0 gcc_trace.txt > vb1.txt
-./sim bimodal 10 0 0 gcc_trace.txt > vb2.txt
-./sim bimodal 5 0 0 jpeg_trace.txt > vb3.txt
-./sim bimodal 6 0 0 perl_trace.txt > vb4.txt
-./sim gshare 10 4 0 0 gcc_trace.txt > vg1.txt
-./sim gshare 14 9 0 0 gcc_trace.txt > vg2.txt
-./sim gshare 11 5 0 0 jpeg_trace.txt > vg3.txt
-./sim gshare 10 6 0 0 perl_trace.txt > vg4.txt
-./sim hybrid 9 14 11 6 0 0 gcc_trace.txt >vh1.txt
-./sim hybrid 5 10 8 5 0 0 jpeg_trace.txt >vh2.txt
-./sim bimodal 7 2048 4 gcc_trace.txt >vbtb1.txt
-./sim gshare 10 6 2048 4 perl_trace.txt >vbtb2.txt
+#!/bin/bash
 
-diff -iyw --suppress-common-lines vb1.txt val_bimodal_1.txt
-diff -iyw --suppress-common-lines vb2.txt val_bimodal_2.txt
-diff -iyw --suppress-common-lines vb3.txt val_bimodal_3.txt
-diff -iyw --suppress-common-lines vb4.txt val_bimodal_4.txt
-diff -iyw --suppress-common-lines vg1.txt val_gshare_1.txt
-diff -iyw --suppress-common-lines vg2.txt val_gshare_2.txt
-diff -iyw --suppress-common-lines vg3.txt val_gshare_3.txt
-diff -iyw --suppress-common-lines vg4.txt val_gshare_4.txt
-diff -iyw --suppress-common-lines vh1.txt val_hybrid_1.txt
-diff -iyw --suppress-common-lines vh2.txt val_hybrid_2.txt
-diff -iyw --suppress-common-lines vbtb1.txt val_BTB_1.txt
-diff -iyw --suppress-common-lines vbtb2.txt val_BTB_2.txt
+cd trace
+../sim bimodal 7 0 0 gcc_trace.txt > vb1.txt
+../sim bimodal 10 0 0 gcc_trace.txt > vb2.txt
+../sim bimodal 5 0 0 jpeg_trace.txt > vb3.txt
+../sim bimodal 6 0 0 perl_trace.txt > vb4.txt
+../sim gshare 10 4 0 0 gcc_trace.txt > vg1.txt
+../sim gshare 14 9 0 0 gcc_trace.txt > vg2.txt
+../sim gshare 11 5 0 0 jpeg_trace.txt > vg3.txt
+../sim gshare 10 6 0 0 perl_trace.txt > vg4.txt
+../sim hybrid 9 14 11 6 0 0 gcc_trace.txt >vh1.txt
+../sim hybrid 5 10 8 5 0 0 jpeg_trace.txt >vh2.txt
+../sim bimodal 7 2048 4 gcc_trace.txt >vbtb1.txt
+../sim gshare 10 6 2048 4 perl_trace.txt >vbtb2.txt
+
+diff -iyw --suppress-common-lines vb1.txt ../testcase/val_bimodal_1.txt
+diff -iyw --suppress-common-lines vb2.txt ../testcase/val_bimodal_2.txt
+diff -iyw --suppress-common-lines vb3.txt ../testcase/val_bimodal_3.txt
+diff -iyw --suppress-common-lines vb4.txt ../testcase/val_bimodal_4.txt
+diff -iyw --suppress-common-lines vg1.txt ../testcase/val_gshare_1.txt
+diff -iyw --suppress-common-lines vg2.txt ../testcase/val_gshare_2.txt
+diff -iyw --suppress-common-lines vg3.txt ../testcase/val_gshare_3.txt
+diff -iyw --suppress-common-lines vg4.txt ../testcase/val_gshare_4.txt
+diff -iyw --suppress-common-lines vh1.txt ../testcase/val_hybrid_1.txt
+diff -iyw --suppress-common-lines vh2.txt ../testcase/val_hybrid_2.txt
+diff -iyw --suppress-common-lines vbtb1.txt ../testcase/val_BTB_1.txt
+diff -iyw --suppress-common-lines vbtb2.txt ../testcase/val_BTB_2.txt
 
 rm -f vb1.txt vb2.txt vb3.txt vb4.txt vg1.txt vg2.txt vg3.txt vg4.txt vh1.txt vh2.txt vbtb1.txt vbtb2.txt
+cd ../
